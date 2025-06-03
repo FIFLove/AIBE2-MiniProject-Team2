74645f20e75b60dfabc604e0e19a0f9edf9f4042
gyuuu9933 <gyuuu9933@gmail.com>
2025-06-03 22:22:22 +0900

    게시글 서비스 구현
    기존 예제 memberService를 기반으로 게시글 서비스 작성

---
+++ src/main/java/hello/hellospring/service/PostService.java
@@ -0,0 +1,34 @@
+package hello.hellospring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import hello.hellospring.domain.Post;
+import hello.hellospring.repository.PostRepository;
+
+@Service
+@Transactional
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Long save(Post post) {
+        postRepository.save(post);
+        return post.getId();
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+
+    public Post findOne(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+} 
